c80eef5fbdff29fd1d1e5b4794f9b64299c6e887
creewick <ilia_Y@mail.ru>
2025-11-13 15:41:56 +0500

    icons

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,12 @@
 import "./Contact.css";
 
 function Contact({ link, title }: { link: string, title: string }) {
+    const domain = new URL(link).hostname;
+    const icon = `/icons/${domain}.png`;
     return (
         <a className="contact" href={link} target="_blank" rel="noopener noreferrer">
-            {title}
+            <img src={icon} className="icon" />
+            <span>{title}</span>
         </a>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
 import Badge from "../components/Badge";
+import Contact from "../components/Contact";
+
+interface Contact {
+    title: string;
+    link: string;
+}
 
 function Home() {
+    const [contacts, setContacts] = useState<Contact[]>([]);
+
+    useEffect(() => void import("../content/socials.json")
+        .then((result) => 
+            setContacts(result.default.socials as Contact[])), []);
+
     return (
         <div>
             <Badge />
+            <div className="contacts">
+                { contacts.map((contact) => (
+                    <Contact 
+                        key={contact.title} 
+                        title={contact.title} 
+                        link={contact.link}
+                    />
+                )) }
+            </div>
         </div>
     )
 }
